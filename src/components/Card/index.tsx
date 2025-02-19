a6f2346ae35a0198961acbe1bfc1fcc9d12dc449
PavloPopovV <pavlopopov570@gmail.com>
2025-02-19 15:44:02 +0200

    style: update styles for Kanban card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,36 +1,42 @@
 import { getDaysAgo } from "../../utils/getDaysAgo"
 import { CardProps } from "../../types/cardTypes"
 
-const Card = ({ task, taskIndex, boardIndex, setActiveCard, setSourceColumnIndex }: CardProps) => {
-  if (!task) return null;
+const Card = ({
+  task,
+  taskIndex,
+  boardIndex,
+  setActiveCard,
+  setSourceColumnIndex,
+}: CardProps) => {
+  if (!task) return null
 
   const handleDragStart = (columnIndex: number, cardIndex: number) => {
-    setActiveCard(cardIndex);
-    setSourceColumnIndex(columnIndex);
-  };
+    setActiveCard(cardIndex)
+    setSourceColumnIndex(columnIndex)
+  }
 
   return (
     <li
-      className="p-3 border border-black rounded-xl bg-white cursor-grab"
+      className="p-3 border border-black rounded-xl bg-white cursor-grab text-[12px]"
       draggable
-      onDragStart={() => handleDragStart(boardIndex, taskIndex)  }
+      onDragStart={() => handleDragStart(boardIndex, taskIndex)}
       onDragEnd={() => setActiveCard(null)}
     >
       <a href={task.html_url} target="_blank">
-
-      <h4 className="font-bold overflow-hidden whitespace-nowrap text-ellipsis mb-1">
-        {task.title}
-      </h4>
+        <h4 className="font-bold overflow-hidden whitespace-nowrap text-ellipsis mb-1">
+          {task.title}
+        </h4>
       </a>
-      <div className="flex gap-4 mb-1 flex-wrap text-gray-500">
+      <div className="flex gap-x-4 mb-1 flex-wrap text-gray-500">
         <span>#{task.number}</span>
         <span>{getDaysAgo(task.updated_at)}</span>
       </div>
-      <div className="flex gap-4 flex-wrap text-gray-500">
+      <div className="flex gap-x-4 flex-wrap text-gray-500">
         <span className="max-w-[120px] overflow-hidden whitespace-nowrap text-ellipsis">
           {task.user.login}
         </span>
-        |<span>Comments: {task.comments}</span>
+        <span className="hidden md:block">|</span>
+        <span>Comments: {task.comments}</span>
       </div>
     </li>
   )
